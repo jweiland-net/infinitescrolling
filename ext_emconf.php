cfc8a7e65a3afb7721330aea13f538570256e5fd
Stefan Froemken <froemken@gmail.com>
2021-09-15 08:20:39 +0200

    Update version to 3.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "infinitescrolling".
- *
- * Auto generated 12-04-2013 17:06
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Infinite Scrolling',
     'description' => 'With this extension you can read nearly every paginator and realize infinite scrolling',
@@ -18,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '3.1.1',
+    'version' => '3.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-10.4.99',
+            'typo3' => '9.5.18-11.5.99',
         ],
         'conflicts' => [
         ],
