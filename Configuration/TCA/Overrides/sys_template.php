99258bbc0d00e8dc6ea337bd07104253fbb38dba
Stefan Froemken <sfroemken@jweiland.net>
2023-05-03 08:44:27 +0200

    Change TYPO3_MODE constant to TYPO3

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'infinitescrolling',
     'Configuration/TypoScript',
